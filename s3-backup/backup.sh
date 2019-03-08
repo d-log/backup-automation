93754db39d6227e01db919e553b87e2179385e39
Marcus Chiu <marcuschiu9@gmail.com>
2019-03-07 20:32:47 -0600

    updated log format

--- s3-backup/backup.sh
+++ s3-backup/backup.sh
@@ -5,8 +5,11 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPT_PATH=$(dirname "$SCRIPT")
 
-printf "\n\nBacking Up  s3://repo.marcuschiu.com"
+printf "\n\nBacking Up  s3://repo.marcuschiu.com\n"
 
 aws s3 sync s3://repo.marcuschiu.com $SCRIPT_PATH/repo.marcuschiu.com
 
 timedatectl | grep "Local time: " > $SCRIPT_PATH/last-backup.txt
+
+printf "\n"
+
